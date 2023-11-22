c8fc7db1b6595323f341aa910415bc0a088328ba
Adam Olsen <olsen.adam06@gmail.com>
2023-11-22 12:34:13 -0700

    Generate a bunch of stats

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import random
+import math
 
-class Stats:
-
+class athleteStats:
+    def __init__(self,weights,posID):
+        self.weightLengths = [2,3,2,2,2] #Number of stats that fall under the category (pace has 2,shooting has 3, etc...)
+        self.numOfStats = 0
+        for elm in self.weightLengths:
+            self.numOfStats += elm
+        self.weights = weights
+        self.posID = posID
+        self.genPlayerStats()
+        
+    def genPlayerStats(self):
+        self.statList = []
+        for n in range(self.numOfStats):
+            self.statList.append(round(random.gauss(60, 15),1))
+            
+        n = 0
+        self.finalWeights = []
+        
+        for wgtLenNum in range(len(self.weightLengths)):
+            for m in range(n, n+self.weightLengths[wgtLenNum]):
+                self.finalWeights.append(self.weights[wgtLenNum])
+                n += 1
+                
+        for m in range(len(self.finalWeights)):
+            self.statList[m] = min(round(self.statList[m] * self.finalWeights[m]),99)
+            
+    def printStats(self):
+        print(self.statList)
+        
 
 class Athlete:
     def __init__(self, id, firstName, lastName):
@@ -22,22 +50,26 @@ class Athlete:
         # Defending: Strength / Ball Stealing
         
         #Weights:
-        #  Goalie:  | Diving |  Handling   | Kicking |  Speed  | Positioning |
-        #  Athlete: |  Pace  |   Shooting  | passing |Dribbling|Defending|
+        #  Goalie:  | Diving |  Handling   | Kicking |  Speed  |Positioning|
+        #  Athlete: |  Pace  |   Shooting  | passing |Dribbling|Defending |
         #          [Acc,speed,power,acc,pen,shrt,long,agil,ctrl,strn,steal]
-        # forWeight = [1.0,1.0,1.0,1.0,1.0,0.8,0.75,0.8,0.75,0.4,0.3]
-        self.goalieWeight = [0.90,0.90,0.90,0.4,0.90]
-        self.forWeight = [0.95,1.00,0.75,0.85,0.40]
-        self.midWeight = [0.90,0.85,1.00,0.85,0.60]
-        self.defWeight = [0.80,0.40,0.85,0.85,1.00]
+        self.goalieWeight = [0.90,0.90,0.90,0.6,0.90]
+        self.forWeight = [0.95,1.00,0.75,0.85,0.60]
+        self.midWeight = [0.90,0.85,1.00,0.85,0.80]
+        self.defWeight = [0.80,0.60,0.85,0.85,1.00]
          
-        if self.
-         
-        self.stats = {}
-        self.stats["Accel"] = random.uniform
-    
+        match self.position:
+            case "Forward":
+                self.stats = athleteStats(self.forWeight,self.posID)
+            case "Midfield":
+                self.stats = athleteStats(self.midWeight,self.posID)
+            case "Defender":
+                self.stats = athleteStats(self.defWeight,self.posID)
+            case "Goaltender":
+                self.stats = athleteStats(self.goalieWeight,self.posID)
+
     def getPosition(self):
-        self.posList = ["Forward","Midfielder","Defender","Goaltender"]
+        self.posList = ["Forward","Midfield","Defender","Goaltender"]
         randNum = random.randint(0,100)
         if randNum < 30: return self.posList[0]
         elif randNum < 60: return self.posList[1]
@@ -48,7 +80,8 @@ class Athlete:
         return self.posList.index(self.position)
     
     def printInfo(self):
-        print("id: %4d | %12s %-12s | pos: %-10s (%d)" % (self.id, self.firstName, self.lastName, self.position, self.posID))
+        print("id: %4d | %12s %-12s | pos: %-10s (%d) " % (self.id, self.firstName, self.lastName, self.position, self.posID), end="")
+        self.stats.printStats()
         
     
 def getNameList():
