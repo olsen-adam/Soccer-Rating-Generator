b469e8e13205d81ac1155e7821d6f035bcdc77d8
Adam Olsen <olsen.adam06@gmail.com>
2023-11-24 12:43:51 -0700

    Created the final Rating System

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 import math
+import time
+import csv  
 
 class athleteStats:
     def __init__(self,weights,posID):
@@ -14,7 +16,7 @@ class athleteStats:
     def genPlayerStats(self):
         self.statList = []
         for n in range(self.numOfStats):
-            self.statList.append(round(random.gauss(60, 15),1))
+            self.statList.append(round(random.gauss(75, 15),1))
             
         n = 0
         self.finalWeights = []
@@ -25,11 +27,25 @@ class athleteStats:
                 n += 1
                 
         for m in range(len(self.finalWeights)):
-            self.statList[m] = min(round(self.statList[m] * self.finalWeights[m]),99)
+            self.statList[m] = max(round(random.gauss(20,5)),min(round(self.statList[m] * self.finalWeights[m]),99))
+            
+        self.oldFinalRating = 40
+        for m in range(len(self.finalWeights)):
+            self.oldFinalRating += self.statList[m]
             
     def printStats(self):
-        print(self.statList)
+        print("Raw Rating: %d Final Rating: %d" % (self.oldFinalRating,self.newFinalRating),self.statList)
         
+    def getOldRating(self):
+        return self.oldFinalRating
+        
+    def adjustFinal(self,min,max,minRating,maxRating):
+        # { [ (final - min) / (max - min) ] * (maxRating - minRating) } + minRating
+        #           a               b                    c                     
+        a = self.oldFinalRating - min
+        b = max - min
+        c = maxRating - minRating
+        self.newFinalRating = (((a/b)*c)+minRating)
 
 class Athlete:
     def __init__(self, id, firstName, lastName):
@@ -53,10 +69,10 @@ class Athlete:
         #  Goalie:  | Diving |  Handling   | Kicking |  Speed  |Positioning|
         #  Athlete: |  Pace  |   Shooting  | passing |Dribbling|Defending |
         #          [Acc,speed,power,acc,pen,shrt,long,agil,ctrl,strn,steal]
-        self.goalieWeight = [0.90,0.90,0.90,0.6,0.90]
-        self.forWeight = [0.95,1.00,0.75,0.85,0.60]
-        self.midWeight = [0.90,0.85,1.00,0.85,0.80]
-        self.defWeight = [0.80,0.60,0.85,0.85,1.00]
+        self.goalieWeight = [0.90,0.90,0.90,0.8,0.90]
+        self.forWeight = [0.95,1.00,0.90,0.90,0.80]
+        self.midWeight = [0.90,0.95,1.00,0.90,0.85]
+        self.defWeight = [0.85,0.85,0.90,0.85,1.00]
          
         match self.position:
             case "Forward":
@@ -67,6 +83,9 @@ class Athlete:
                 self.stats = athleteStats(self.defWeight,self.posID)
             case "Goaltender":
                 self.stats = athleteStats(self.goalieWeight,self.posID)
+
+    def adjustStats(self,min,max,minRating,maxRating):
+        self.stats.adjustFinal(min,max,minRating,maxRating)
 
     def getPosition(self):
         self.posList = ["Forward","Midfield","Defender","Goaltender"]
@@ -104,17 +123,38 @@ def getNameList():
         lnList[n] = lnList[n].strip()
     
     return fnList, lnList
-        
+
+def writeToFile(listOfAthletes):
+    try:
+        nameOfFile = "playerList.csv"
+        writeMode = 'w'
+        with open(nameOfFile,writeMode) as csvFile:
+                fileWriter = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                fileWriter.writerow(["FirstName","LastName","Position","FinalRating","Acc","Speed","Power","Accuracy","Penalty","ShortPass","LongPass","Agility","Control","Strength","Stealing"])
+    except:
+        print("Failed to create CSV File.")
+        return
+    finally: 
+        print("\nSuccessfully Created CSV File (%s)" % nameOfFile)
 def main():
     fnList, lnList = getNameList()
-    numOfAthletes = 1000
+    numOfAthletes = 10000
     listOfAthletes = []
     
     for n in range(numOfAthletes):
         listOfAthletes.append(Athlete(n, random.choice(fnList), random.choice(lnList)))
 
+    finalStats = []
     for athlete in listOfAthletes:
+        finalStats.append(athlete.stats.getOldRating())
+
+    minRating = 40
+    maxRating = 99
+    for athlete in listOfAthletes:
+        athlete.adjustStats(min(finalStats),max(finalStats),minRating,maxRating)
         athlete.printInfo()
+        
+    writeToFile(listOfAthletes)
     
         
 if __name__ == "__main__":
